e9783bb4ee742a6ac49cddf176cd1d733a70666b
Zsuzsa.L <manoaway@gmail.com>
2024-02-02 22:30:46 +0100

    fix day6.1

--- day6.py
+++ day6.py
@@ -2,28 +2,17 @@ import sqlite3
 
 conn = sqlite3.connect('Day6.db')
 
-print("\n6.1 List all the scientists' names, their projects' names and the total hours worked on each project in alphabetical order of scientist name")
-res1=conn.execute('''SELECT Scientists.name, Projects.name, Projects.hours
+print("\n6.1 List all the scientists' names, their projects' names and the total hours worked on each project in alphabetical order of project name than scientist name")
+res1=conn.execute('''SELECT Projects.name, Scientists.name,  Projects.hours
                     FROM AssignedTo
                     JOIN Scientists
                     ON  Scientists.SSN=AssignedTo.Scientist
                     JOIN Projects 
                      ON Projects.Code=AssignedTo.Project
-                    ORDER BY Scientists.name''')
+                    ORDER BY Projects.name, Scientists.name''')
 for row in res1:
    print(row)
    
-
-print("\nin alphabetical order of project name")
-res1=conn.execute('''SELECT Projects.name, Scientists.name, Projects.hours
-                    FROM AssignedTo
-                    JOIN Scientists
-                    ON  Scientists.SSN=AssignedTo.Scientist
-                    JOIN Projects 
-                    ON Projects.Code=AssignedTo.Project
-                    ORDER BY Projects.name''')
-for row in res1:
-   print(row)
    
 print("\n6.2 Select the project names which are not assigned yet")
 res2=conn.execute('''SELECT Projects.name
