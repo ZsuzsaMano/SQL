52fd121c25c4ac1d2df0e68f54ae6b7b3626dcb0
Zsuzsa.L <manoaway@gmail.com>
2024-02-08 15:06:38 +0100

    finish day8

--- day8.py
+++ day8.py
@@ -2,24 +2,83 @@ import sqlite3
 
 conn = sqlite3.connect('Day8.db')
 
-# print("\n 8.1 Obtain the names of all physicians that have performed a medical procedure they have never been certified to perform.")
-# res1 = conn.execute('''SELECT name, procedure
-#                        FROM Physician 
-#                        JOIN Undergoes
-#                        ON Physician.EmployeeID=Undergoes.Physician
-#                        WHERE procedure IN (SELECT  treatment
-#                        FROM Physician 
-#                        JOIN Trained_In
-#                        ON Physician.EmployeeID=Trained_In.Physician
-#                        )
-#                       ''')
-# for row in res1:
-#    print(row) 
+print("\n 8.1 Obtain the names of all physicians that have performed a medical procedure they have never been certified to perform.")
+res1 = conn.execute('''SELECT DISTINCT Physician.name, Undergoes.procedure
+                        FROM Physician 
+                        JOIN Undergoes 
+                        ON Physician.EmployeeID = Undergoes.Physician
+                        WHERE Undergoes.procedure NOT IN (
+                           SELECT Trained_In.treatment
+                           FROM Trained_In
+                           WHERE Trained_In.Physician = Physician.EmployeeID);
+                                          ''')
+for row in res1:
+   print(row) 
    
-# print("\n 8.2 Same as the previous query, but include the following information in the results: Physician name, name of procedure, date when the procedure was carried out, name of the patient the procedure was carried out on.")
-# print("\n 8.3 Obtain the names of all physicians that have performed a medical procedure that they are certified to perform, but such that the procedure was done at a date (Undergoes.Date) after the physician's certification expired (Trained_In.CertificationExpires).")
-# print("\n 8.4 Same as the previous query, but include the following information in the results: Physician name, name of procedure, date when the procedure was carried out, name of the patient the procedure was carried out on, and date when the certification expired.")
-# print("\n 8.5 Obtain the information for appointments where a patient met with a physician other than his/her primary care physician. Show the following information: Patient name, physician name, nurse name (if any), start and end time of appointment, examination room, and the name of the patient's primary care physician.")
+   
+print("\n 8.2 Same as the previous query, but include the following information in the results: Physician name, name of procedure, date when the procedure was carried out, name of the patient the procedure was carried out on.")
+res2 = conn.execute('''SELECT DISTINCT Physician.name, procedures.name, Undergoes.date, patient.name
+                        FROM Physician 
+                        JOIN Undergoes 
+                        ON Physician.EmployeeID = Undergoes.Physician
+                        JOIN procedures
+                        ON undergoes.procedure=procedures.code
+                        JOIN patient
+                        ON Undergoes.patient=patient.ssn
+                        WHERE Undergoes.procedure NOT IN (
+                           SELECT Trained_In.treatment
+                           FROM Trained_In
+                           WHERE Trained_In.Physician = Physician.EmployeeID);
+                                          ''')
+for row in res2:
+   print(row) 
+
+print("\n 8.3 Obtain the names of all physicians that have performed a medical procedure that they are certified to perform, but such that the procedure was done at a date (Undergoes.Date) after the physician's certification expired (Trained_In.CertificationExpires).")
+res3 = conn.execute('''SELECT Physician.name, procedure, treatment, undergoes.date, trained_in.CertificationExpires
+                       FROM Physician
+                       JOIN Undergoes
+                       ON Physician.EmployeeID=Undergoes.Physician
+                       JOIN Trained_In
+                       ON Physician.EmployeeID=Trained_In.Physician 
+                       WHERE treatment=procedure   
+                       AND DATE(undergoes.date) > DATE(trained_in.CertificationExpires)          
+                      ''')
+for row in res3:
+   print(row) 
+
+print("\n 8.4 Same as the previous query, but include the following information in the results: Physician name, name of procedure, date when the procedure was carried out, name of the patient the procedure was carried out on, and date when the certification expired.")
+res3 = conn.execute('''SELECT Physician.name, procedures.name, undergoes.date, patient.name, trained_in.CertificationExpires
+                       FROM Physician
+                       JOIN Undergoes
+                       ON Physician.EmployeeID=Undergoes.Physician
+                       JOIN Trained_In
+                       ON Physician.EmployeeID=Trained_In.Physician 
+                       JOIN procedures
+                       ON undergoes.procedure=procedures.code
+                       JOIN patient
+                       ON Undergoes.patient=patient.ssn
+                       WHERE treatment=procedure   
+                       AND DATE(undergoes.date) > DATE(trained_in.CertificationExpires)          
+                      ''')
+for row in res3:
+   print(row) 
+
+print("\n 8.5 Obtain the information for appointments where a patient met with a physician other than his/her primary care physician. Show the following information: Patient name, physician name, nurse name (if any), start and end time of appointment, examination room, and the name of the patient's primary care physician.")
+res5 = conn.execute('''SELECT patient.name,Physician.name,Nurse.name, start, end, examinationRoom, PCP.name
+                       FROM Appointment
+                       JOIN Patient
+                       ON Appointment.Patient=Patient.SSN
+                       JOIN Physician
+                       ON Physician.EmployeeID=Appointment.Physician  
+                       JOIN Physician as PCP
+                       ON PCP.EmployeeID=Patient.PCP
+                       LEFT JOIN Nurse
+                       ON Appointment.PrepNurse=Nurse.EmployeeID
+                       WHERE PCP!=Appointment.Physician
+                      ''')
+for row in res5:
+   print(row) 
+
 print("\n 8.6 The Patient field in Undergoes is redundant, since we can obtain it from the Stay table. There are no constraints in force to prevent inconsistencies between these two tables. More specifically, the Undergoes table may include a row where the patient ID does not match the one we would obtain from the Stay table through the Undergoes.Stay foreign key. Select all rows from Undergoes that exhibit this inconsistency. ")
 res6 = conn.execute('''SELECT *
                        FROM Undergoes
