81d4405503969836e1921d2951bd76b2bd2050a5
Zsuzsa.L <manoaway@gmail.com>
2024-02-08 10:56:42 +0100

    more tasks from day8

--- day8.py
+++ day8.py
@@ -20,9 +20,42 @@ conn = sqlite3.connect('Day8.db')
 # print("\n 8.3 Obtain the names of all physicians that have performed a medical procedure that they are certified to perform, but such that the procedure was done at a date (Undergoes.Date) after the physician's certification expired (Trained_In.CertificationExpires).")
 # print("\n 8.4 Same as the previous query, but include the following information in the results: Physician name, name of procedure, date when the procedure was carried out, name of the patient the procedure was carried out on, and date when the certification expired.")
 # print("\n 8.5 Obtain the information for appointments where a patient met with a physician other than his/her primary care physician. Show the following information: Patient name, physician name, nurse name (if any), start and end time of appointment, examination room, and the name of the patient's primary care physician.")
-# print("\n 8.6 The Patient field in Undergoes is redundant, since we can obtain it from the Stay table. There are no constraints in force to prevent inconsistencies between these two tables. More specifically, the Undergoes table may include a row where the patient ID does not match the one we would obtain from the Stay table through the Undergoes.Stay foreign key. Select all rows from Undergoes that exhibit this inconsistency. ")
-# print("\n 8.7 Obtain the names of all the nurses who have ever been on call for room 123.")
-# print("\n 8.8 The hospital has several examination rooms where appointments take place. Obtain the number of appointments that have taken place in each examination room. ")
+print("\n 8.6 The Patient field in Undergoes is redundant, since we can obtain it from the Stay table. There are no constraints in force to prevent inconsistencies between these two tables. More specifically, the Undergoes table may include a row where the patient ID does not match the one we would obtain from the Stay table through the Undergoes.Stay foreign key. Select all rows from Undergoes that exhibit this inconsistency. ")
+res6 = conn.execute('''SELECT *
+                       FROM Undergoes
+                       JOIN Stay
+                       ON Stay.StayID=Undergoes.Stay
+                       WHERE stay.patient!=undergoes.patient       
+                      ''')
+for row in res6:
+   print(row) 
+
+print("\n 8.7 Obtain the names of all the nurses who have ever been on call for room 123.")
+res7 = conn.execute('''SELECT  Nurse.name, Room.Number
+                       From Room
+                       JOIN Block 
+                       ON Room.BlockFloor=Block.Floor
+                       AND Room.BlockCode=Block.Code
+                       JOIN On_Call
+                       ON On_Call.BlockFloor=Block.Floor
+                       AND On_Call.BlockCode=Block.Code
+                       JOIN Nurse
+                       ON On_Call.Nurse=Nurse.EmployeeID  
+                       WHERE Room.Number=123     
+                      ''')
+
+for row in res7:
+   print(row) 
+
+
+print("\n 8.8 The hospital has several examination rooms where appointments take place. Obtain the number of appointments that have taken place in each examination room. ")
+res8 = conn.execute('''SELECT ExaminationRoom, COUNT(ExaminationRoom)
+                       FROM Appointment
+                       GROUP BY ExaminationRoom                     
+                      ''')
+for row in res8:
+   print(row) 
+
 print("\n 8.9 Obtain the names of all patients and their primary care physician, such that the following are true:")
 print("\n 8.9.1 The patient has been prescribed some medication by his/her primary care physician.")
 res9 = conn.execute('''SELECT patient.name,  Physician.name, PCP.name
